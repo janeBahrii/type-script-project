b7ec9cd84f7437b985cd7d8045c7ea844e5cc59a
Yevheniia Bahrii <Jnezhil@gmail.com>
2021-04-12 12:21:12 +0300

    Add root element,validate

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,29 @@
 // Code goes here!
+
+enum validType {
+    vNumber,
+    vText
+};
+
+const rootElement = document.getElementById("app")! as HTMLElement;
+
+const idFormTemplate = document.querySelector("#project-input")! as HTMLTemplateElement;
+const form = idFormTemplate.content.cloneNode(true)! as HTMLElement;
+//console.log(document.getElementById("project-input"));
+rootElement.append(form);
+
+function validate(value: any, type: validType): boolean {
+    let regex: RegExp;
+    switch (type) {
+        case validType.vNumber:
+            regex = new RegExp("^\d+$");
+            if (regex.test(value))
+                return true;
+            return false;
+        case validType.vText:
+            return true;
+
+    }
+    return true;
+}
+
